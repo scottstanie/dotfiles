dc6993aa69c03109d4c6f2d487e5ec9d442302be
scott <scott.stanie@gmail.com>
2017-09-27 13:38:01 -0500

    add xclip to install

--- install_list.sh
+++ install_list.sh
@@ -5,7 +5,7 @@
 
 sudo apt-get update
 
-for package in anki apt-file curl cython3 deja-dup-backend-gvfs fdupes gfortran git gnome-tweak-tool google-chrome-stable htop lynx m4 python3-gdal python3-pip python-pip scons skypeforlinux slack-desktop texlive-full texstudio tomate-gtk tomate-indicator-plugin vim vim-gnome; do
+for package in anki apt-file curl cython3 deja-dup-backend-gvfs fdupes gfortran git gnome-tweak-tool google-chrome-stable htop lynx m4 python3-gdal python3-pip python-pip scons skypeforlinux slack-desktop texlive-full texstudio tomate-gtk tomate-indicator-plugin vim vim-gnome xclip; do
   sudo apt-get -y install $package
 done
 
