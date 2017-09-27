28e7cb2c8afe3df4dac0d11dbcc7bb3a1f871ead
scott <scott.stanie@gmail.com>
2017-09-27 13:20:30 -0500

    create bash script for apt installs

---
+++ install_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Note: I got these from running the following:
+# comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)
+
+sudo apt-get update
+
+for package in anki apt-file curl cython3 deja-dup-backend-gvfs fdupes gfortran git gnome-tweak-tool google-chrome-stable htop lynx m4 python3-gdal python3-pip python-pip scons skypeforlinux slack-desktop texlive-full texstudio tomate-gtk tomate-indicator-plugin vim vim-gnome; do
+  sudo apt-get -y install $package
+done
+
